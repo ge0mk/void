74f94e4591f9c94171b4027c93e92a602cea9d2a
Georg Kunze <georgm.kunze@gmail.com>
2023-05-22 13:58:42 +0200

    stdlib: fix libc.c not aborting on panic

--- libc.c
+++ libc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
@@ -18,6 +19,7 @@ void error_panic(int error) {
 	fwrite(prefix, 1, strlen(prefix), stderr);
 	fwrite(msg, 1, strlen(msg), stderr);
 	fputc('\n', stderr);
+	abort();
 }
 
 int* get_errno() {
