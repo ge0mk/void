b073e6d15f5daad0d9a7386358fd08aaa2050741
Georg Kunze <georgm.kunze@gmail.com>
2024-05-17 17:44:01 +0200

    compiler: update llvm binding

--- src/llvm.h
+++ src/llvm.h
@@ -1,18 +1,20 @@
 // used to generate the llvm binding
 // put all required llvm headers here and run `./binding_generator.py selfhost/llvm.h selfhost/llvm.vd` to generate selfhost/llvm.vd
 
-#include <llvm-c/Types.h>
-#include <llvm-c/Core.h>
-#include <llvm-c/ErrorHandling.h>
-#include <llvm-c/Initialization.h>
 #include <llvm-c/Analysis.h>
-#include <llvm-c/Target.h>
-#include <llvm-c/IRReader.h>
 #include <llvm-c/BitReader.h>
 #include <llvm-c/BitWriter.h>
-#include <llvm-c/Linker.h>
+#include <llvm-c/Core.h>
+#include <llvm-c/DataTypes.h>
 #include <llvm-c/DebugInfo.h>
+#include <llvm-c/Error.h>
+#include <llvm-c/ErrorHandling.h>
 #include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/IRReader.h>
+#include <llvm-c/Linker.h>
+#include <llvm-c/Object.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/Types.h>
 
 enum DWARFTag {
 #define HANDLE_DW_TAG(ID, NAME, VERSION, VENDOR, KIND) Tag_##NAME = ID,
@@ -25,5 +27,3 @@ enum DWARFTypeEncoding {
 
 #include <llvm/BinaryFormat/Dwarf.def>
 };
-
-
